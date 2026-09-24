c3a0301b4a5c6a450b3440f97e6a7a101b44e3d0
magaybi178381-afk <magaybi178381@me.bergen.edu>
2026-09-24 09:06:41 -0400

    Add files via upload

---
+++ MemoryPool.h
@@ -0,0 +1,14 @@
+class MemoryPool
+{
+public:
+    MemoryPool(size_t blockSize, size_t blockCount);
+    ~MemoryPool();
+
+    void* allocate();
+    bool deallocate(void* ptr);
+
+    size_t availableBlocks() const;
+    size_t allocatedBlocks() const;
+    size_t blockSize() const;
+    size_t capacity() const;
+};
