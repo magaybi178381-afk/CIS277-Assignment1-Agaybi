6bb4a8f402507498fb28fb490ae3424600f1e26b
magaybi178381-afk <magaybi178381@me.bergen.edu>
2026-09-24 09:48:57 -0400

    Implement dynamic Stack class with basic operations
    Refactored Stack class to manage dynamic memory and added push, pop, top, empty, and size methods.

--- Stack.h
+++ Stack.h
@@ -1,9 +1,66 @@
+#include <cstddef>
+
 template <typename T>
 class Stack
-{
-    void push(const T& value);
-    T pop();
-    T& top();
-    bool empty() const;
-    size_t size() const;
+{   
+Private:
+    T* data;
+    size_t capacity;
+    size_t count;
+    
+Public:
+    Stack()
+    {
+        data = nullptr;
+        capacity = 0;
+        count = 0;
+    }
+
+    ~Stack()
+    {
+        delete[] data;
+    }
+
+    void push(const T& value)
+    {
+        if(count ==  capacity)
+        {
+            size_t newCap = capacity + 1;
+            T* newData = new T[newCap];
+
+            for(size_t = 0; i <count; i++)
+            {
+                newData[i] = data[i];
+            }
+            
+            delete[] data;
+            data = newData;
+            capacity = newCap;
+
+        }
+
+        data[count] = value;
+        count++;
+    }
+
+    T pop()
+    {
+        count--;
+        return data[count];
+    }
+    
+    T& top()
+    {
+        data[count-1];
+    }
+
+    bool empty() const
+    {
+        return count == 0;
+    }
+
+    size_t size() const
+    {
+        return count;
+    }
 }
